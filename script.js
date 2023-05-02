5ac1b4e15610a0996db396687825a586eddfb530
vcorneau <vcorneau@gmail.com>
2023-05-02 15:15:10 -0400

    Add functions for each operation

---
+++ script.js
@@ -0,0 +1,19 @@
+// add function
+function add(a,b) {
+    return a+b;
+}
+
+// subtract function
+function sub(a,b) {
+    return a-b;
+}
+
+// multiply function
+function mult(a,b) {
+    return a*b;
+}
+
+// divide function
+function div(a,b) {
+    return a/b;
+}
