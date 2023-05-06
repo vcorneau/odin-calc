28915e385e1375e59abd470f916a1c04c21d7b29
vcorneau <vcorneau@gmail.com>
2023-05-06 13:00:19 -0400

    Fix formatting

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-//Function to calculate based on input
+// Function to calculate based on input
 
 function operate(operator,firstNumber,secondNumber) {
     let a = parseInt(firstNumber);
@@ -39,7 +39,7 @@ function operate(operator,firstNumber,secondNumber) {
     }
 }
 
-//On button click, add numbers to buffer
+// On button click, add numbers to buffer
 
 let newInputBuffer = [];
 let calcBuffer = [];
@@ -50,96 +50,90 @@ let currentDisplay;
 
 let buttons = document.getElementsByClassName("button");
 
-//Receives user input and puts numbers and decimal in a buffer
+// Receives user input and puts numbers and decimal in a buffer
 function receiveFirstUserInput(event) {
-    if (event.target.id === "button1") {
-        newInputBuffer.push(1);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button2") {
-        newInputBuffer.push(2);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button3") {
-        newInputBuffer.push(3);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button4") {
-        newInputBuffer.push(4);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button5") {
-        newInputBuffer.push(5);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button6") {
-        newInputBuffer.push(6);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button7") {
-        newInputBuffer.push(7);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button8") {
-        newInputBuffer.push(8);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button9") {
-        newInputBuffer.push(9);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "button0") {
-        newInputBuffer.push(0);
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
-    } else if (event.target.id === "buttonDec") {
-        if(newInputBuffer.includes(".")){
-            return;
-        }
-        newInputBuffer.push(".");
-        calcBuffer = newInputBuffer.join('');
-        firstNumber = parseFloat(calcBuffer);
-        console.log(firstNumber);
+  if (event.target.id === "button1") {
+    newInputBuffer.push(1);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+  } else if (event.target.id === "button2") {
+    newInputBuffer.push(2);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+  } else if (event.target.id === "button3") {
+    newInputBuffer.push(3);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button4") {
+    newInputBuffer.push(4);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button5") {
+    newInputBuffer.push(5);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button6") {
+    newInputBuffer.push(6);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button7") {
+    newInputBuffer.push(7);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button8") {
+    newInputBuffer.push(8);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button9") {
+    newInputBuffer.push(9);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "button0") {
+    newInputBuffer.push(0);
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+} else if (event.target.id === "buttonDec") {
+    if(newInputBuffer.includes('.')) {
+      return;
     }
-    display.innerHTML = firstNumber;
+    newInputBuffer.push('.');
+    calcBuffer = newInputBuffer.join('');
+    firstNumber = parseFloat(calcBuffer);
+    console.log(firstNumber);
+}
+display.innerHTML = firstNumber;
 }
 
 function resetData() {
-    newInputBuffer = [];
-    calcBuffer = [];
-    operatorBuffer = [];
-    firstNumber = null;
-    display.innerHTML = ""; 
-
+  newInputBuffer = [];
+  calcBuffer = [];
+  operatorBuffer = [];
+  firstNumber = null;
+  display.innerHTML = '';
 }
 
 for (let button of buttons) {
-    button.addEventListener("click", receiveFirstUserInput);
+  button.addEventListener('click', receiveFirstUserInput);
 }
 
-buttonClear.addEventListener("click", resetData);
+buttonClear.addEventListener('click', resetData);
 
-//Display number to be added in display
+//  Display number to be added in display
 
+//  Store first number into variable when operator is pressed and clear display.
+//  First number will be stored in an array, concat the array, then parseInt
 
-
-//Store first number into variable when operator is pressed and clear display.
-//First number will be stored in an array, concat the array, then parseInt
-
-//Store second number when operator is pressed and clear display
+//  Store second number when operator is pressed and clear display
 //
 
-//Display result when = is pressed
-
-
-
+//  Display result when = is pressed
