c53fc17628941ce06f50a71e8462f26a493a6b83
vcorneau <vcorneau@gmail.com>
2023-05-09 10:45:32 -0400

    Nothing is working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let leftNumber
 let rightNumber
 let operator
 
-let newInputBuffer = []
+let newInputBuffer = [1, 2]
 let calcBuffer = []
 let operatorBuffer = []
 
@@ -12,23 +12,35 @@ let currentDisplay
 
 let buttons = document.getElementsByClassName('button')
 
-function processUserInput () {
+// Parse a float from the input buffer and return that value
+function toFloat(newInputBuffer) {
+  newInputBuffer = []
+  calcBuffer = newInputBuffer.join('')
+  let float = parseFloat(calcBuffer)
+  return float
+}
+
+function receiveUserInput(leftNumber, rightNumber) {
+  for (let button of buttons) {
+    button.addEventListener('click', function (element) {
+      if(element.target.class === number
+
+    })
+  }
+}
+
+// Function takes number and puts it in one of two variables
+function processUserInput (newInputBuffer, calcBuffer, operatorBuffer) {
   newInputBuffer.push(Element.innerHTML)
   calcBuffer = newInputBuffer.join('')
 
-  if (leftNumber === null && EventTarget.class === 'number') {
+  if (leftNumber === null) {
     leftNumber = parseFloat(calcBuffer)
     return leftNumber
   }
 
   rightNumber = parseFloat(calcBuffer)
   return rightNumber
-}
-
-function getUserInput(leftNumber, rightNumber) {
-  for (let button of buttons) {
-    button.addEventListener('click', userInput)
-  }
 }
 
 
