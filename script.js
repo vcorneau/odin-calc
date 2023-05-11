5d70b4baa63125da2c7cb27811e2e994aea8b3cb
vcorneau <vcorneau@gmail.com>
2023-05-11 15:43:41 -0400

    Swith case for  button

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-let buttons = document.getElementsByClassName('button')
-
+// Function with switch case to push inputs to buffer. Doesn't currently work.
 function pushToInputBuffers() {
+  const buttons = document.getElementsByClassName('button')
   const newInputBuffer = []
-  let numberBuffer = []
-  let number
-  let operator
+  for (const button of buttons) {
+    button.addEventListener('click', function() {
+      const buttonClass = button.classList
+      let numberBuffer = []
+      let number
+      let operator
 
-  switch (EventTarget.class) {
-    case 'number':
-      newInputBuffer.push(Element.innerHTML)
-      numberBuffer = newInputBuffer.join('')
-      number = parseFloat(numberBuffer)
-      console.log(number)
-      return number
-    case 'operator':
-      operator = Element.innerHTML
-      console.log(operator)
-      return operator
+      switch (true) {
+        case buttonClass.contains('number'):
+          newInputBuffer.push(button.innerHTML)
+          numberBuffer = newInputBuffer.join('')
+          number = parseFloat(numberBuffer)
+          console.log(number)
+          return number
+        case buttonClass.contains('operator'):
+          operator = button.innerHTML
+          console.log(operator)
+          return operator
+      }
+    }
+    )
   }
 }
-
-for (let button of buttons) {
-  button.addEventListener('click', pushToInputBuffers)
-}
+pushToInputBuffers()
