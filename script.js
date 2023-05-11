ff9cd90959f2a952ecfd8fdf3ce39314f8270203
vcorneau <vcorneau@gmail.com>
2023-05-11 16:37:26 -0400

    Clear data after operator

--- script.js
+++ script.js
@@ -19,36 +19,44 @@ const buttons = document.getElementsByClassName('button')
 let buttonClass
 let newInputBuffer = []
 let numberBuffer = []
+let leftNumber
+let rightNumber
 let number
 let operator
+const display = document.getElementById('display')
+let decimalUsed = false
 
 function resetData() {
   newInputBuffer = []
   numberBuffer = []
   number = null
   operator = null
+  decimalUsed = false
   display.innerHTML = ''
 }
-
 
 for (const button of buttons) {
   button.addEventListener('click', function() {
     buttonClass = button.classList
 
-    switch (true) {
-      case buttonClass.contains('number'):
-        newInputBuffer.push(button.innerHTML)
-        numberBuffer = newInputBuffer.join('')
-        number = parseFloat(numberBuffer)
-        display.innerHTML = number
-        break
-      case buttonClass.contains('operator'):
-        operator = button.innerHTML
-        console.log(operator)
-        break
-      case buttonClass.contains('clear'):
-        resetData()
-        break
+    if (buttonClass.contains('number')) {
+      newInputBuffer.push(button.innerHTML)
+      numberBuffer = newInputBuffer.join('')
+      number = parseFloat(numberBuffer)
+      display.innerHTML = number
+    } else if (buttonClass.contains('decimal') && decimalUsed === false) {
+      newInputBuffer.push(button.innerHTML)
+      numberBuffer = newInputBuffer.join('')
+      number = parseFloat(numberBuffer)
+      decimalUsed = true
+      display.innerHTML = number
+    } else if (buttonClass.contains('operator')) {
+      operator = button.innerHTML
+      leftNumber = number
+      resetData()
+    } else if (buttonClass.contains('clear')) {
+      resetData()
     }
   })
 }
+
