aefd0f7d4a51d622a38917cc88edaa8057e9ab9a
vcorneau <vcorneau@gmail.com>
2023-05-11 16:06:46 -0400

    Progress on switch case

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-// Function with switch case to push inputs to buffer. Doesn't currently work.
-function pushToInputBuffers() {
-  const buttons = document.getElementsByClassName('button')
-  const newInputBuffer = []
-  for (const button of buttons) {
-    button.addEventListener('click', function() {
-      const buttonClass = button.classList
-      let numberBuffer = []
-      let number
-      let operator
-
-      switch (true) {
-        case buttonClass.contains('number'):
-          newInputBuffer.push(button.innerHTML)
-          numberBuffer = newInputBuffer.join('')
-          number = parseFloat(numberBuffer)
-          console.log(number)
-          return number
-        case buttonClass.contains('operator'):
-          operator = button.innerHTML
-          console.log(operator)
-          return operator
-      }
-    }
-    )
-  }
+// Operation functions
+function add(leftNumber, rightNumber) {
+  return leftNumber + rightNumber
 }
-pushToInputBuffers()
+function sub(leftNumber, rightNumber) {
+  return leftNumber - rightNumber;
+}
+function mult(leftNumber, rightNumber) {
+  return leftNumber*rightNumber;
+}
+function div(leftNumber, rightNumber) {
+  return leftNumber / rightNumber;
+
+const buttons = document.getElementsByClassName('button')
+const newInputBuffer = []
+}
+
+const buttons = document.getElementsByClassName('button')
+let buttonClass = []
+let newInputBuffer = []
+let numberBuffer = []
+let number
+
+
+for (const button of buttons) {
+  button.addEventListener('click', function() {
+    buttonClass = button.classList
+    console.log(buttonClass)
+  })
+}
+
+switch (true) {
+  case buttonClass.contains('number'):
+    newInputBuffer.push(button.innerHTML)
+    numberBuffer = newInputBuffer.join('')
+    number = parseFloat(numberBuffer)
+    console.log(number)
+    break
+  case buttonClass.contains('operator'):
+    operator = button.innerHTML
+    console.log(operator)
+    break
+  case buttonClass.contains('clear'):
+    resetData
+    console.log(number)
+    break
+}
+
+
